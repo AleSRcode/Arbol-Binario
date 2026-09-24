a9bbfbcb1e10b698a539e2be516d61d70208d36c
acostachristian <christianjavieracosta@outlook.com>
2026-09-23 20:03:28 -0400

    Hice el Metodo de Encontrar Nodo Para Reemplazar para la funcion de eliminar

--- Arbol Binario/Programa.cpp
+++ Arbol Binario/Programa.cpp
@@ -9,6 +9,30 @@ struct Nodo {
 	Nodo* derecha;
 };
 
+Nodo* EncontrarNodoParaReemplazar(Nodo* nodoAEliminar) //|||SOLO USAR SI EL NodoAEliminar TIENE HIJOS|||
+{
+	Nodo* nodoBusqueda = NULL;
+	if (nodoAEliminar -> derecha != NULL) {
+
+		nodoBusqueda = nodoAEliminar -> derecha;
+
+		while (nodoBusqueda->izquierda != NULL)
+		{
+			nodoBusqueda = nodoBusqueda -> izquierda;
+		}
+	}
+	else if (nodoAEliminar -> izquierda != NULL) {
+
+		nodoBusqueda = nodoAEliminar -> izquierda;
+
+		while (nodoBusqueda->derecha != NULL)
+		{
+			nodoBusqueda = nodoBusqueda -> derecha;
+		}
+	}
+		return nodoBusqueda;
+}
+
 int main()
 {
   return 0;
